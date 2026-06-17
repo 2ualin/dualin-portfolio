e9bfb1119e6d41b30540663ebbc6d3a3e7475455
2ualin <2ualin@gmail.com>
2026-06-17 16:18:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const cards = document.querySelectorAll(".card");
 
+// 카드 & 챕터 스르륵 등장 효과
 const observer = new IntersectionObserver((entries)=>{
   entries.forEach(entry=>{
+
     if(entry.isIntersecting){
       entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
     }
+
   });
 },{
-  threshold:0.2
+  threshold:0.3
 });
+
+
 
 cards.forEach(card=>{
   observer.observe(card);
